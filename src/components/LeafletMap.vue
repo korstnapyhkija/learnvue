0fb2352f2f5de0256774bca2949ae9a0be95bf93
Annabel Nigu <MM-20-627@tptlive.ee>
2021-11-17 10:38:30 +0200

    tups

---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import axios from 'axios';
+export default {
+    props: ['lat','lng','zoom','geoJson'],
+    mounted(){
+        this.map = L.map('map').setView([this.lat, this.lng], this.zoom);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: "pk.eyJ1Ijoia29yc3RuYXB5aGtpamEiLCJhIjoiY2t3MzdxYjB5MjJsaDJucWlndTFyMTY1ciJ9.Ei9oRvvv8ADJqR00Lcnfqw"
+        }).addTo(this.map);
+            L.geoJson(this.geoJson).addTo(this.map);
+    },
+    data(){
+        return{
+            map:null
+        }
+    },
+    watch: {
+        lat(lat){
+            this.map.flyTo([lat, this.lng], this.zoom);
+        },
+        lng(lng){
+            this.map.flyTo([this.lat, lng], this.zoom);
+        },
+        zoom(zoom){
+            this.map.flyTo([this.lat, this.lng], zoom);
+        }
+    }
+}
+</script>
+
+<style>
+    #map { 
+        height: 800px; }
+</style>
--- src/components/MapExample.vue
+++ src/components/MapExample.vue
@@ -1,14 +1,23 @@
 <template>
 <div>
     <button class="button is-primary" @click="goToHome">Go to home</button>
-    <google-maps :lat="this.lat" :lng="this.lng" :zoom="this.zoom"></google-maps>
+    <!-- <google-maps :lat="this.lat" :lng="this.lng" :zoom="this.zoom"></google-maps> -->
+    <leaflet-map :lat="lat" :lng="lng" :zoom="zoom"></leaflet-map>
 </div>
 </template>
 
 <script>
 import GoogleMaps from './GoogleMaps.vue'
+import LeafletMap from './LeafletMap.vue'
 export default {
-  components: { GoogleMaps },
+  components: { GoogleMaps, LeafletMap },
+    created(){
+        let url = "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json";
+        axios.get(url).then(response =>{
+            console.log(response.data);
+            this.geoJson = response.data;
+        });
+    },
     data(){
         return {
             lat: 59.4265182,
